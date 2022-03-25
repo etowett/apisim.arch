2e096e06646995a581e969986ebc3ddf4988cf52
Eutychus Towett <ektowett@gmail.com>
2022-03-25 13:30:51 +0300

    upgrade golang to v1.18 and added more details to healthz endpoint

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -7,6 +7,7 @@ import (
 	"apisim/app/models"
 	"encoding/json"
 	"fmt"
+	"os"
 	"runtime"
 	"time"
 
@@ -33,19 +34,70 @@ func (c App) Dash() revel.Result {
 	return c.Render()
 }
 
+func (c App) bytesToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
 func (c App) Health() revel.Result {
+	ctx := c.Request.Context()
+
+	hostName, err := os.Hostname()
+	if err != nil {
+		c.Log.Errorf("could not get hostname: %v", err)
+	}
+
+	dbUp := true
+	err = db.DB().Ping()
+	if err != nil {
+		dbUp = false
+		c.Log.Errorf("db ping failed because err=[%v]", err)
+	}
+
+	hello, err := (&models.Checker{}).One(ctx, db.DB())
+	if err != nil {
+		c.Log.Errorf("could not db select 1: %v", err)
+	}
+
+	version, err := (&models.Checker{}).Version(ctx, db.DB())
+	if err != nil {
+		c.Log.Errorf("could not db version: %v", err)
+	}
+
 	memStats := &runtime.MemStats{}
 	runtime.ReadMemStats(memStats)
+
+	currentTime := time.Now()
+	tZone, offset := currentTime.Zone()
+
 	return c.RenderJSON(map[string]interface{}{
-		"success":     true,
-		"status":      "Ok",
-		"server_time": time.Now(),
-		"version":     app.AppVersion,
-		"build_time":  app.BuildTime,
+		"success": true,
+		"time": map[string]interface{}{
+			"now":      currentTime,
+			"timezone": tZone,
+			"offset":   offset,
+		},
+		"version":    app.AppVersion,
+		"build_time": app.BuildTime,
+		"db": map[string]interface{}{
+			"type":    "postgres",
+			"up":      dbUp,
+			"hello":   hello,
+			"version": version,
+		},
+
 		"server": map[string]interface{}{
+			"hostname":   hostName,
 			"cpu":        runtime.NumCPU(),
 			"goroutines": runtime.NumGoroutine(),
-			"memory":     memStats.Alloc,
+			"goarch":     runtime.GOARCH,
+			"goos":       runtime.GOOS,
+			"compiler":   runtime.Compiler,
+			"memory": map[string]interface{}{
+				"alloc":       fmt.Sprintf("%v MB", c.bytesToMb(memStats.Alloc)),
+				"total_alloc": fmt.Sprintf("%v MB", c.bytesToMb(memStats.TotalAlloc)),
+				"sys":         fmt.Sprintf("%v MB", c.bytesToMb(memStats.Sys)),
+				"num_gc":      memStats.NumGC,
+			},
 		},
 	})
 }
--- go.mod
+++ go.mod
@@ -1,20 +1,36 @@
 module apisim
 
-go 1.16
+go 1.18
 
 require (
-	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b // indirect
-	github.com/garyburd/redigo v1.6.2 // indirect
-	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gocraft/work v0.5.1
-	github.com/gomodule/redigo v1.8.4
-	github.com/google/uuid v1.2.0
-	github.com/lib/pq v1.10.2
-	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.8
+	github.com/google/uuid v1.3.0
+	github.com/lib/pq v1.10.4
 	github.com/revel/modules v1.0.0
 	github.com/revel/revel v1.0.0
-	github.com/robfig/cron v1.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
-	golang.org/x/text v0.3.6
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064
+	golang.org/x/text v0.3.7
 	gopkg.in/guregu/null.v4 v4.0.0
+)
+
+require (
+	github.com/bradfitz/gomemcache v0.0.0-20220106215444-fb4bf637b56d // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/garyburd/redigo v1.6.3 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/inconshreveable/log15 v0.0.0-20200109203555-b30bc20e4fd1 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/revel/config v0.21.0 // indirect
+	github.com/revel/log15 v2.11.20+incompatible // indirect
+	github.com/revel/pathtree v0.0.0-20140121041023-41257a1839e9 // indirect
+	github.com/robfig/cron v1.2.0 // indirect
+	github.com/twinj/uuid v1.0.0 // indirect
+	github.com/xeonx/timeago v1.0.0-rc4 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/stack.v0 v0.0.0-20141108040640-9b43fcefddd0 // indirect
 )
